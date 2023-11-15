969781847cdb2a2256ea0fc8ed8f1ede91ac3c54
ElenaFoster <99051673+ElenaFoster@users.noreply.github.com>
2023-11-15 07:55:10 -0600

    Update Auto_2023.java

--- Auto_2023.java
+++ Auto_2023.java
@@ -6,24 +6,34 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior; 
+//NEED TO IMPORT MATH AND COS/SIN
+import com.qualcomm.robotcore.cos;
+
 
 @Autonomous(name="Auto_2023", group = "OpMode" )
 public class Auto_2023 extends LinearOpMode {
-   DcMotor Liftmotor;
-   DcMotor motorTop;
-   DcMotor motorRR;
-   DcMotor motorRF;
-   Servo Pullservo;
-   Servo Spinservo;
+   private DcMotor Liftmotor;
+   private DcMotor motorTop;
+   private DcMotor motorRR;
+   private DcMotor motorRF;
+   private CRServo Pullservo;
+   private Servo Spinservo;
+   private ColorSensor colorSensor;
+
+    int r;
+    int g;
+    int b;
   
   public void runOpMode() throws InterruptedException {
+      colorSensor = hardwareMap.colorSensor.get("colorSensor");
       motorLR = hardwareMap.dcMotor.get("motorTop");
       motorLF = hardwareMap.dcMotor.get("motorRF");
       motorRR = hardwareMap.dcMotor.get("motorRR");
       Liftmotor = hardwareMap.dcMotor.get("Liftmotor");
-      PullServo = hardwareMap.servo.get("PullServo");
-      Spinservo = hardwareMap.crservo.get("Spinservo");
+      PullServo = hardwareMap.crservo.get("PullServo");
+      Spinservo = hardwareMap.servo.get("Spinservo");
       
       telemetry.addData("Mode", "New");
       telemetry.update();
@@ -31,9 +41,49 @@ public class Auto_2023 extends LinearOpMode {
       //ConeServo.setPower(.6);
       
       waitForStart();
-      
-      telemetry.addData("Mode", "New");
-      telemetry.update();
+             
+        //green(200,600,400)
+        //oragne(750,740,300)
+        //purple(400,450,700)
+        
+        telemetry.addData("r", r);
+        telemetry.addData("g", g);
+        telemetry.addData("b", b);
+        telemetry.update();
+
+      //Collect the RGB values 
+        r = colorSensor.red();
+        g = colorSensor.green();
+        b = colorSensor.blue();
+
+     //drives forward to middle spik
+     {
+        motorTop.setPower(0);
+        motorRR.setPower(sin(60));
+        motorRL.setPower(sin(120));
+        sleep(300)
+      }
+
+     //drops purple pixel on middle spik or flips servo and then drops
+      if ((r<g) && (g<b) && (r<b)){
+         telemetry.addLine("purple");
+         Pullservo.setPower(-.5);
+         sleep(100)
+         Spinservo.setPosition(180)
+      }else{
+         Pullservo.setPower(0);
+         Spinservo.setPosition(0);
+         sleep(1000)
+      }
+
+     //reverses the robot
+      }
+         motorTop.setPower(0);
+         motorRR.setPower(sin(-60));
+         motorRL.setPower(sin(-120));
+
+     
+
 
 
 
